d1dd494bf39803cc0b43ac11d7f213b6397c0c4d
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-02-24 15:51:39 -0600

    remove .join method per assignment requirements

--- js/app.js
+++ js/app.js
@@ -7,12 +7,10 @@ const testButton = document.querySelector("button");
 function checkAnswer(userAnswer, correctAnswers) {
   userAnswer = userAnswer.toLowerCase();
   if (correctAnswers.includes(userAnswer)) {
-    // console.log("correct");
-    alert("correct");
+    alert("Correct!");
     return true;
   } else {
-    // console.log("incorrect");
-    alert("incorrect");
+    alert("Sorry, wrong answer!");
     return false;
   } 
 }
@@ -24,13 +22,12 @@ function displayWelcomeName(name) {
 
 function checkGuess(guess, answer) {
   guess = parseInt(guess);
-  // console.log(guess);
   if ( isNaN(guess) )  {
     alert("Must enter valid number!");
     return false;
   }
   if (guess == answer) {
-    alert("you got it!");
+    alert("You got it!");
     return true;
   } else if (guess < answer) {
     alert("Too low!");
@@ -44,12 +41,12 @@ function checkGuess(guess, answer) {
 let userName = prompt("Hello, what is your name?");
 displayWelcomeName(userName);
 
-// event listeners
+// event listener for quiz pop take quiz button
 testButton.addEventListener("click", (evnt) => {
   alert(
     `Thanks ${
       userName ? userName : "user"
-    }, please answer a 'y' or 'n' to all of the following questions`
+    }, Please answer a 'yes'(y) or 'no'(n) to the next 5 five questions`
   );
   let correctAnswers = 0;
 
@@ -111,18 +108,22 @@ testButton.addEventListener("click", (evnt) => {
 
   counter = 6;
   for (let i = 0; i < 6; i++) {
-    let correct = false;
     let place = prompt("Name one of my 10 favorite places in memphis");
-    if (place === null) {
-      place = "";
-    }
+    let correct = false;
+
+    // prompt returns null if user hit cancel
+    if (place === null) place = "";
 
     let strippedPlace = place.toLowerCase().replace(/ /g, '');
     for(let j = 0; j < answerArr.length; j++) {
       if (answerArr[j] == strippedPlace) {
         correctAnswers++;
         correct = true;
-        alert(`${place} is correct! I enjoy all of these places in memphis:\n\n${placeArr.join(", ")}`);
+        let responseString = 'Correct! The other possible correct answers are:\n\n';
+        for(let i = 0; i < placeArr.length; i++) {
+        responseString += `${placeArr[i]}, `;
+        }
+        alert(responseString);
         break;
       } 
     }
@@ -135,7 +136,11 @@ testButton.addEventListener("click", (evnt) => {
   }
 
   if(counter == 0) {
-    alert(`sorry the possible correct answers are:\n\n ${placeArr.join(', ')}`)
+    let responseString = 'sorry the possible correct answers are:\n\n';
+    for(let i = 0; i < placeArr.length; i++) {
+      responseString += `${placeArr[i]}, `;
+    }
+    alert(responseString);
   }
 
   // final game alert
